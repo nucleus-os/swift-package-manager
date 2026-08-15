0c38aba1f03d0bb1847234adf9d08b76d665a1ca
Maddy <maddythewisp@gmail.com>
2026-08-14 20:50:26 -0600

    Build pinned Nucleus SwiftPM overlay

--- Package.swift
+++ Package.swift
@@ -1189,7 +1189,9 @@ if !shouldUseSwiftBuildFramework {
 
     if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
         package.dependencies += [
-            .package(url: "https://github.com/swiftlang/swift-build.git", branch: relatedDependenciesBranch),
+            .package(
+                url: "https://github.com/nucleus-os/swift-build.git",
+                revision: "ab1492cad8de4d3f8d76f3e8f774c9c4247c4140"),
             .package(url: "https://github.com/swiftlang/swift-tools-protocols.git", branch: relatedDependenciesBranch),
         ]
     } else {
