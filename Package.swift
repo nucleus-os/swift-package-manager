b5b8220e3f57420542db0cda2f8109568b41f931
Maddy <maddythewisp@gmail.com>
2026-08-15 23:39:28 -0600

    Advance architecture-safe SwiftBuild dependency reuse

--- Package.swift
+++ Package.swift
@@ -1191,7 +1191,7 @@ if !shouldUseSwiftBuildFramework {
         package.dependencies += [
             .package(
                 url: "https://github.com/nucleus-os/swift-build.git",
-                revision: "17d7eadc96b92fda4ee7cc2340dd1cb4998e2260"),
+                revision: "e58dc939e858faf110fe2c723b7043aa9f44b53c"),
             .package(url: "https://github.com/swiftlang/swift-tools-protocols.git", branch: relatedDependenciesBranch),
         ]
     } else {
